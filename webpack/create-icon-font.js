5a387c087e6461b0b13c712d6b8939c7127b2ad6
Vadim <vadiqwe@yandex.ru>
2021-03-26 01:28:30 +0300

    change icon-font generator plugin config for scss support

--- webpack/create-icon-font.js
+++ webpack/create-icon-font.js
@@ -9,8 +9,8 @@ module.exports = function (__rootPath) {
       'comrade-builder/icon-fonts/icon-font-template.njk'
     ),
     svgs: path.resolve(__rootPath, 'src/assets/icons/*.svg'),
-    fontsOutput: path.resolve(__rootPath, 'src/assets/fonts/'),
-    cssOutput: path.resolve(__rootPath, 'src/styles/modules/icon-font.css'),
+    fontsOutput: path.join(__rootPath, 'src', 'assets', 'fonts'),
+    cssOutput: path.join(__rootPath, 'src', 'styles', 'modules', 'icon-font.scss'),
     htmlOutput: false,
     formats: ['woff2']
   })
